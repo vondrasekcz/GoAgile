6c2b56d54439ed13160c682d5a1735dc0c3f19fa
pavel <pavel@PAVEL-THINK>
2016-10-23 22:56:03 +0200

    Add project files.

---
+++ GoAgile/Controllers/RetrospectiveController.cs
@@ -0,0 +1,33 @@
+﻿using GoAgile.Models;
+using System.Web.Mvc;
+
+namespace GoAgile.Controllers
+{
+    public class RetrospectiveController : Controller
+    {
+        //
+        // GET Retrospecive/CreateRetrospective
+        [Authorize]
+        public ActionResult CreateRetrospective()
+        {
+            ViewBag.Message = "Create Retrospecive.";
+
+            return View();
+        }
+
+        //
+        // POST Retrospective/CreateRerospective
+        [HttpPost]
+        [Authorize]
+        public ActionResult CreateRetrospective(CreateRetrospectiveViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
+    }
+
+
+}
---
+++ GoAgile/Models/ViewModels/RetrospectiveViewModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GoAgile.Models
+{
+    public class CreateRetrospectiveViewModel
+    {
+        [Required]
+        [Display(Name = "Retrospective Name")]
+        [StringLength(maximumLength: 30)]
+        public string RetrospectiveName { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 30)]
+        public string Project { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Start Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime StartDate { get; set; }
+
+        [StringLength(maximumLength: 30)]
+        public string Comment { get; set; }
+    }
+}
